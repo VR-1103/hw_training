0b49001c477eccbdb4c2e35365e8bab59ea2841f
VR-1103 <96605598+VR-1103@users.noreply.github.com>
2022-06-18 21:05:51 +0530

    Editing autonomous.py

--- autonomous.py
+++ autonomous.py
@@ -20,8 +20,24 @@ class Drive:
         """TODO - INSERT YOUR CODE HERE
         
         Problem Statement: Insert Code here to make the rover move 
-        forwards, backwards, left, right according to input given
+        forwards, Backwards, left, right according to input given
         """
+        if (axes[1] >= 0.1):
+            driver1.ForwardM1(axes[1]*100/0.9)
+            driver1.ForwardM2(axes[1]*100/0.9)
+            driver2.ForwardM1(axes[1]*100/0.9)
+            driver2.ForwardM2(axes[1]*100/0.9)
+        elif (axes[1] <= -0.1):
+            driver1.BackwardM1(axes[1]*100/0.9)
+            driver1.BackwardM2(axes[1]*100/0.9)
+            driver2.BackwardM1(axes[1]*100/0.9)
+            driver2.BackwardM2(axes[1]*100/0.9)
+        elif (axes[0] <= -0.1):
+            driver1.TurnRightMixed(axes[0]*100/0.9)
+            driver2.TurnRightMixed(axes[0]*100/0.9)
+        elif (axes[0] >= 0.1):
+            driver1.TurnLeftMixed(axes[0]*100/0.9)
+            driver2.TurnLeftMixed(axes[0]*100/0.9)
         
 
     def current_limiter(self):
